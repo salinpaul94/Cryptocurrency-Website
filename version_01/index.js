27134acb1ae41b4f760110316628d990e1dba466
salinpaul valooran <salinpv@hotmail.com>
2023-11-17 00:34:10 +0530

    jquery ajax function to fetch data from coingecko api

--- version_01/index.js
+++ version_01/index.js
@@ -3,3 +3,8 @@ let settings = {
   scrossDomain: true,
   url: "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin%2C%20ethereum%2C%20dogecoin&vs_currencies=inr",
 };
+
+
+$.ajax(settings).done(function(response){
+  console.log(response);
+})
